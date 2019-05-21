93ee5ed1219bc7fcaa3b3ed9439e28298b06b4b1
Graham Barber <grahammbarber@gmail.com>
2019-05-21 09:14:41 -0700

    feat(root): add global styles, MDXProvider

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import { styled } from 'linaria/react'
+import { MDXProvider } from '@mdx-js/react'
+
 import Document, { tableOfContents } from '../documents.index'
+
+const Global = styled.div`
+  :global() {
+    html {
+      box-sizing: border-box;
+    }
+
+    *,
+    *:after,
+    *:before {
+      box-sizing: inherit;
+    }
+
+    body {
+      margin: 0;
+    }
+  }
+`
 
 export const Root = () => {
   return (
     <>
-      <p>{JSON.stringify(tableOfContents)}</p>
-      <Document />
+      <Global />
+      <p>{JSON.stringify(tableOfContents())}</p>
+      <MDXProvider>
+        <Document />
+      </MDXProvider>
     </>
   )
 }
