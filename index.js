5afbcfed23ae158340e90b979a8233603ec49050
Graham Barber <grahammbarber@gmail.com>
2019-05-21 13:43:23 -0700

    feat(index): use progress spinners, config function

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 
+const ora = require('ora')
+const path = require('path')
 const webpack = require('webpack')
 const { readFile } = require('fs').promises
-const path = require('path')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const schema = require('./schema')
-const StaticReactSiteGeneratorPlugin = require('./plugin')
-
-const presets = ['@babel/preset-env', '@babel/preset-react', 'linaria/babel']
+const buildConfig = require('./buildConfig')
 
 ;(async function () {
+  const configSpinner = ora('Validating config...').start()
   let config
+
   try {
     let fileContents = await readFile(
       path.resolve(process.cwd(), 'spectri.json'),
@@ -20,130 +19,22 @@ const presets = ['@babel/preset-env', '@babel/preset-react', 'linaria/babel']
     )
     config = JSON.parse(fileContents)
     await schema.isValid(config)
+    configSpinner.succeed('Config validated')
   } catch (e) {
-    console.error(e)
+    configSpinner.fail(`Config invalid: ${e}`)
     process.exit(1)
   }
 
-  webpack(
-    {
-      context: __dirname,
-      entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-        static: path.resolve(__dirname, './src/static.js')
-      },
-      mode: 'production',
-      output: {
-        filename: '[hash].[name].js',
-        path: path.resolve(process.cwd(), config.dist || 'dist'),
-        libraryTarget: 'umd',
-        globalObject: 'this'
-      },
-      resolve: {
-        extensions: ['.js', '.json', '.jsx', '.mdx', '.md']
-      },
-      module: {
-        rules: [
-          {
-            test: /\.jsx?$/,
-            use: [
-              {
-                loader: 'babel-loader',
-                options: {
-                  presets
-                }
-              },
-              {
-                loader: 'linaria/loader',
-                options: {
-                  sourceMap: true,
-                  displayName: true,
-                  babelOptions: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
-                  }
-                }
-              }
-            ]
-          },
-          {
-            test: /\.css$/,
-            use: [
-              MiniCssExtractPlugin.loader,
-              {
-                loader: 'css-loader',
-                options: {
-                  sourceMap: true
-                }
-              }
-            ]
-          },
-          {
-            test: /\.mdx?$/,
-            use: [
-              {
-                loader: 'babel-loader',
-                options: {
-                  presets
-                }
-              },
-              './loaders/mdx/index.js'
-            ]
-          },
-          {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-              {
-                loader: 'url-loader',
-                options: {
-                  limit: 8192
-                }
-              }
-            ]
-          },
-          {
-            test: /\.(ttf|eot|woff|woff2)$/,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: 'fonts/[name].[ext]'
-                }
-              }
-            ]
-          },
-          {
-            test: /\.index$/,
-            use: [
-              {
-                loader: 'babel-loader',
-                options: {
-                  presets
-                }
-              },
-              {
-                loader: './loaders/documents/index.js',
-                options: {
-                  document: config.entry
-                }
-              }
-            ]
-          }
-        ]
-      },
-      plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({ filename: '[hash].[name].css' }),
-        new StaticReactSiteGeneratorPlugin({ title: config.title })
-      ]
-    },
-    (err, stats) => {
-      if (err | stats.hasErrors()) {
-        console.log('Errors: ', err)
-        console.log('Stat Errors: ', stats.compilation.errors)
-        throw new Error(err || stats.compilation.errors)
-      }
-
-      console.log('Successfully output files')
+  const buildSpinner = ora('Building files...').start()
+  webpack(buildConfig(config), (err, stats) => {
+    if (err | stats.hasErrors()) {
+      console.error('Errors: ', err)
+      console.error('Stat Errors: ', stats.compilation.errors)
+      buildSpinner.fail('Build unsuccessful')
     }
-  )
+
+    buildSpinner.succeed(
+      `Files available in the "${config.dist || 'dist'}" directory`
+    )
+  })
 })()
