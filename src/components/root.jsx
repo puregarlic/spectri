dac3e9812bbeb2f7762f30fa6ff344c4d996d07e
Graham Barber <grahammbarber@gmail.com>
2019-05-22 16:40:51 -0700

    feat: add fonts, basic styling

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { styled } from 'linaria/react'
+import { Helmet } from 'react-helmet'
 import { MDXProvider } from '@mdx-js/react'
 
 import Document, { tableOfContents } from '../documents.index'
+
+import Footer from './footer'
 
 const Global = styled.div`
   :global() {
     html {
       box-sizing: border-box;
+      font-family: 'Inter', -apple-system, sans-serif;
+      font-size: 20px;
+      line-height: 1.5;
     }
 
     *,
@@ -22,14 +28,40 @@ const Global = styled.div`
   }
 `
 
+const DocumentContainer = styled.div`
+  @media screen {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 64px;
+    padding-bottom: 64px;
+  }
+`
+
+const ContentContainer = styled.div`
+  @media screen {
+    width: 100%;
+    max-width: 32em;
+    padding-bottom: 64px;
+  }
+`
+
 export const Root = () => {
   return (
-    <>
+    <DocumentContainer>
+      <Helmet>
+        <title>Hey there</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
+      </Helmet>
       <Global />
-      <p>{JSON.stringify(tableOfContents())}</p>
-      <MDXProvider>
-        <Document />
-      </MDXProvider>
-    </>
+      <ContentContainer>
+        <MDXProvider>
+          <Document />
+        </MDXProvider>
+      </ContentContainer>
+      <Footer />
+    </DocumentContainer>
   )
 }
