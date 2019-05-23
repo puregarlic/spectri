20558349861e30083dd3dcc5d55fb85f3f3ba56d
Graham Barber <grahammbarber@gmail.com>
2019-05-22 17:37:10 -0700

    feat(image): enable image zooming

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,8 +1,16 @@
-import { styled } from 'linaria/react'
+import React from 'react'
+import { css } from 'linaria'
+import ImageZoom from 'react-medium-image-zoom'
 
-const Image = styled.img`
+const className = css`
   width: 100%;
   height: auto;
 `
+
+const Image = props => (
+  <a href={props.src} onClick={e => e.preventDefault()}>
+    <ImageZoom image={{ className, ...props }} zoomImage={props} />
+  </a>
+)
 
 export default Image
